099e9f1194188d9634ad397359afc26d7729d866
baianjo <1987892914@qq.com>
2025-08-24 17:06:54 +0800

    Upd: first commit

---
+++ analysis.py
@@ -0,0 +1,6 @@
+import pandas as pd # pd 是 pandas 的通用简称，像是一个约定俗成的外号
+import numpy as np # np 是 numpy 的外号
+import matplotlib.pyplot as plt # plt 是画图工具的外号
+import seaborn as sns # sns 是另一个更好看的画图工具的外号
+
+print("所有工具已准备就绪！")
